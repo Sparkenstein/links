49149d1b5a5f60580c751a28be49581a1f7b627f
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-20 22:03:27 +0530

    refactoring

---
+++ api/getHandler.js
@@ -0,0 +1,12 @@
+const client = require("./db");
+
+
+const INDEX = "index";
+const SET_KEY = "link:";
+
+
+async function getHandler(req, res){
+    
+}
+
+module.exports = getHandler;
--- api/router.js
+++ api/router.js
@@ -1,27 +1,11 @@
 const polka = require("polka");
 const router = polka();
 
-const client = require("./db");
-const INDEX = "index";
-const SET_KEY = "link:";
+const postHandler = require("./postHandler");
+const getHandler = require("./getHandler");
 
 router
-    .get("/get", (req, res) => {res.end("Get links")})
-    .post("/post", async (req, res) => {
-      const request = req.body;
-      if(request.flushall){
-        await client.flushall();
-      }
-      const id = await client.incr(INDEX);
-      request.id = id;
-      try {
-        console.log("setting", `${SET_KEY}${id}`, ...Object.entries(request).flat());
-        await client.hset(`${SET_KEY}${id}`, ...Object.entries(request).flat());
-        res.end("Done");
-      } catch (e) {
-        console.error(e);        
-        res.end("error occurred");        
-      }
-    });
+    .get("/get", getHandler)
+    .post("/post", postHandler);
 
 module.exports = router
