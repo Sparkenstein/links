c7ca79bad06c825c7a598b31cacf17ec62f2a219
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-20 16:40:47 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const polka = require("polka");
+const {json} = require("body-parser");
 const routes = require("./api");
 const instance = polka().listen(3000, () => {
     console.log("Server started on port 3000")
 });
 
+instance.use(json());
 instance.use("/api", routes);
-
-    
