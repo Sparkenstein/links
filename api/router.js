6e77e4f33631636fff2da9c5581c2bbd605320ea
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-20 17:34:11 +0530

    updated client, link saving working

--- api/router.js
+++ api/router.js
@@ -2,15 +2,26 @@ const polka = require("polka");
 const router = polka();
 
 const client = require("./db");
+const INDEX = "index";
+const SET_KEY = "link:";
 
-// create urls
 router
     .get("/get", (req, res) => {res.end("Get links")})
     .post("/post", async (req, res) => {
-      await client.incr(req.body.get)
-      const id = await client.get(req.body.get);
-      res.end(`done ${id }`);
-      // db.hset
+      const request = req.body;
+      if(request.flushall){
+        await client.flushall();
+      }
+      const id = await client.incr(INDEX);
+      request.id = id;
+      try {
+        console.log("setting", `${SET_KEY}${id}`, ...Object.entries(request).flat());
+        await client.hset(`${SET_KEY}${id}`, ...Object.entries(request).flat());
+        res.end("Done");
+      } catch (e) {
+        console.error(e);        
+        res.end("error occurred");        
+      }
     });
 
 module.exports = router
