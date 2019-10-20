6f1a8731683e217dd2540aa552881e2df002494f
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-20 22:03:14 +0530

    redis monitor to log queries

--- api/db.js
+++ api/db.js
@@ -1,4 +1,10 @@
 const Redis = require("ioredis");
 const db = new Redis();
 
+
+(async () => {
+    const monitor = await db.monitor();
+    monitor.on('monitor', console.log);
+})()
+
 module.exports = db;
