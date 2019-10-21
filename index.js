fb5bcd1cdfc349bbfca51f7edcf08b95f33d0456
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-21 21:28:59 +0530

    cleanup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { json } = require('body-parser');
 const { join } = require("path");
 const routes = require('./api');
 
-const dir = join(__dirname, 'public');
+const dir = join(__dirname, 'dist');
 const serve = require('serve-static')(dir);
 const instance = polka().listen(3000, () => {
     console.log('Server started on port 3000');
@@ -13,7 +13,4 @@ const instance = polka().listen(3000, () => {
 instance
     .use(json())
     .use(serve)
-    .use('/api', routes)
-    .get('/', (req, res) => {
-        res.end("<h1>Hello Page</h1>")
-    });
+    .use('/api', routes);
