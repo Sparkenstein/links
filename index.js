eccd9e5951f9be1c110153361bae573bb19b988d
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-21 14:21:41 +0530

    serving static pages

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict';
 const polka = require('polka');
 const { json } = require('body-parser');
+const { join } = require("path");
 const routes = require('./api');
+
+const dir = join(__dirname, 'public');
+const serve = require('serve-static')(dir);
 const instance = polka().listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
 instance
     .use(json())
+    .use(serve)
     .use('/api', routes)
     .get('/', (req, res) => {
         res.end("<h1>Hello Page</h1>")
