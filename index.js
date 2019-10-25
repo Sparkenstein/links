d3366202af1cb058cabd4d8a51e2aa41f57c141b
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-25 17:39:35 +0530

    moving to react

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
 const polka = require('polka');
 const { json } = require('body-parser');
-const { join } = require("path");
 const routes = require('./api');
 
-const dir = join(__dirname, 'dist');
-const serve = require('serve-static')(dir);
-const instance = polka().listen(3000, () => {
-    console.log('Server started on port 3000');
+const instance = polka().listen(3001, () => {
+    console.log('Server started on port 3001');
 });
 
 instance
     .use(json())
-    .use(serve)
     .use('/api', routes);
--- public/main.js
+++
@@ -1,6 +0,0 @@
-import * as m from "mithril";
-import 'bulma';
-import { Main } from "./components";
-
-var root = document.body;
-m.mount(root, Main);
