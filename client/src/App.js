6ae189397d76a89f6e9e7645313854f6b5d6139b
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-25 18:12:28 +0530

    updated navbar and cleanups

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Navbar } from 'react-bootstrap';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar bg="bg-white" variant="light">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt="Logo"
+                        src="/logo.png"
+                        width="112"
+                        height="28"
+                        className="d-inline-block align-top"
+                    />
+                    {' React Bootstrap'}
+                </Navbar.Brand>
+            </Navbar>
+        </>
+    );
 }
 
 export default App;
