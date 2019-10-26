f0d4cd9a67fd244e77e847267140b3264fc303ed
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-26 14:58:36 +0530

    testing and validating request before forwarding

--- api/router.js
+++ api/router.js
@@ -9,6 +9,21 @@ const getHandler = require("./getHandler");
 
 router
     .get("/get/:id", getHandler(client))
-    .post("/post", postHandler(client));
+    .post("/post", (req, res) => {
+        const request = req.body;
+        if(!request.link){
+            res.writeHead(400, "Bad Request")
+            res.end('{"message": "URL is missing"}')
+        }
+        if(!request.category){
+            res.writeHead(400, "Bad Request")
+            res.end('{"message": "Category is required"}')
+        }
+        if(!request.tags || request.tags.length <1){
+            res.writeHead(400, "Bad Request")
+            res.end('{"message": "At least 1 tag is required"}')
+        }
+        res.end('{"message": "saved"}')
+    });
 
 module.exports = router
