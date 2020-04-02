88f031160db4a7e45883280ab367e058c133a7ec
Sparkenstein <zetabytes.pp@gmail.com>
2020-04-02 20:18:36 +0530

    [update] removed model

--- public/components/navbar.js
+++ public/components/navbar.js
@@ -1,8 +1,6 @@
 import m from 'mithril';
 import logo from '../logo.png';
 
-let isShowing = false;
-
 export function Navbar() {
     return m(
         'nav',
@@ -12,74 +10,6 @@ export function Navbar() {
             'aria-label': 'main navigation'
         },
         [
-            //modal
-            m('div', { class: `modal ${isShowing && 'is-active'}` }, [
-                m('.modal-background'),
-                m(
-                    '.modal-content',
-                    m('.box', [
-                        m('.columns', [
-                            m(
-                                '.column',
-                                m('.field', [
-                                    m('label.label', 'URL'),
-                                    m(
-                                        '.control',
-                                        m('input', {
-                                            class: 'input is-primary'
-                                        })
-                                    )
-                                ])
-                            ),
-                            m(
-                                '.column',
-                                m('.field', [
-                                    m('label.label', 'Tags'),
-                                    m(
-                                        '.control',
-                                        m('input', {
-                                            class: 'input is-primary'
-                                        })
-                                    )
-                                ])
-                            )
-                        ]),
-                        m('.columns', [
-                            m(
-                                '.column',
-                                m('.field', [
-                                    m('label.label', 'Description'),
-                                    m(
-                                        '.control',
-                                        m('input', {
-                                            class: 'input is-primary'
-                                        })
-                                    )
-                                ])
-                            ),
-                            m(
-                                '.column',
-                                m('.field', [
-                                    m('label.label', 'Category'),
-                                    m(
-                                        '.control',
-                                        m('input', {
-                                            class: 'input is-primary'
-                                        })
-                                    )
-                                ])
-                            )
-                        ])
-                    ])
-                ),
-                m('button', {
-                    class: 'modal-close is-large',
-                    onclick: () => {
-                        isShowing = false;
-                    },
-                    'aria-label': 'close'
-                })
-            ]),
             // Navbar brand
             m('div', { class: 'navbar-brand' }, [
                 m(
@@ -102,31 +32,7 @@ export function Navbar() {
                         m('span', { 'aria-hidden': true })
                     ]
                 )
-            ]),
-            m(
-                'div',
-                { class: 'navbar-menu' },
-                m(
-                    'div',
-                    { class: 'navbar-end' },
-                    m(
-                        'div',
-                        { class: 'navbar-item' },
-                        m(
-                            'div',
-                            {
-                                class: 'button is-primary',
-                                onclick: openModal
-                            },
-                            m('strong', 'Submit')
-                        )
-                    )
-                )
-            )
+            ])
         ]
     );
-}
-
-function openModal() {
-    isShowing = true;
 }
