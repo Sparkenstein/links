a71e780130cf38d0f9a6818063fae0d6781a9555
Sparkenstein <zetabytes.pp@gmail.com>
2020-04-04 07:44:49 +0530

    [WIP] - changing look

--- public/components/body.js
+++ public/components/body.js
@@ -1,25 +1,28 @@
 import m from 'mithril';
 
 export function Body() {
-    return m('div', { class: 'section' }, [
-        m(
-            '.container',
-            m('h1.title', { style: { padding: '10px' } }, 'Search...')
-        ),
-        m(
-            '.container',
+    return m('div', { class: 'columns' }, [
+        m('div', { class: 'section column is-two-thirds box', style: 'height: 92vh;' }, [
             m(
-                '.field',
+                '.container',
+                m('h1.title', { style: { padding: '10px' } }, 'Search...')
+            ),
+            m(
+                '.container',
                 m(
-                    '.control',
-                    m('input', {
-                        class: 'input is-primary is-rounded',
-                        id: 'searchbar',
-                        type: 'text',
-                        placeholder: 'Enter Search String...'
-                    })
+                    '.field',
+                    m(
+                        '.control',
+                        m('input', {
+                            class: 'input is-primary is-rounded',
+                            id: 'searchbar',
+                            type: 'text',
+                            placeholder: 'Enter Search String...'
+                        })
+                    )
                 )
             )
-        )
+        ]),
+        m('div', { class: 'column' }, m('div', { class: 'section' }, 'Hello'))
     ]);
 }
